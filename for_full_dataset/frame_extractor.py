e0122593f0a6ea179e30e6ed02f143264ecd11db
Piyush Bhatt <sharingan@vpn-128-194-199-8.tamulink.tamu.edu>
2018-12-11 23:56:43 +0530

    full dataset CNN

---
+++ for_full_dataset/frame_extractor.py
@@ -0,0 +1,65 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+import cv2
+
+
+# In[37]:
+
+
+sessions = ['Session1', 'Session2', 'Session3', 'Session4', 'Session5']
+segment_loc = "/Users/sharingan/Documents/IEMOCAP_vid_clips/"
+frames_loc = "/Users/sharingan/Documents/IEMOCAP_vid_frames/"
+emotion_classes = ['ang', 'hap', 'neu']
+
+
+# In[48]:
+
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+for sess in sessions:
+    
+    # make directory for each session
+    os.system('mkdir ' + frames_loc + sess)
+    for emo in emotion_classes:
+        
+        if os.path.isdir(segment_loc + sess + '/' + emo):
+            video_files = [x for x in os.listdir(segment_loc + sess + '/' + emo) if x[-3:] == 'avi']
+#             video_files = [x.split('_')[1][:-2] for x in video_files]
+            # make directories for each emotion class inside all sessions
+            out_loc = frames_loc + sess + '/' + emo
+            os.system('mkdir ' + out_loc)
+            
+            for vid in video_files:
+                if vid.split('_')[1][:-2] == 'script':
+                    video = cv2.VideoCapture(segment_loc + sess + '/' + emo + '/' + vid)
+                    print vid, video.isOpened(),
+                    framerate = video.get(5)
+
+                    while (video.isOpened()):
+                        frameId = video.get(1)
+                        success,image = video.read()
+
+                        if(success != True):
+                            break
+
+                        # crop and resize the image
+                        image = image[120:360, 50:290]
+                        image = cv2.resize(image, (50, 50), interpolation=cv2.INTER_AREA)
+                        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #                         imgplot = plt.imshow(image)
+    #                         plt.show()
+
+                        # save the frame
+                        filename = out_loc + '/' + vid[:-4] + '_' + str(int(frameId)) + '.jpg'
+                        cv2.imwrite(filename,image)
+                    video.release()
+                    print('-->done')
+        else:
+            print "Empty"
+
---
+++ for_full_dataset/segment_video.py
@@ -0,0 +1,69 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import cv2
+
+
+# In[21]:
+
+
+dataset_dir = '/Users/sharingan/Documents/IEMOCAP_full_release/'
+sessions = ['Session1', 'Session2', 'Session3', 'Session4', 'Session5']
+vid_loc = '/dialog/avi/DivX/'
+
+datafile_loc = '/dialog/EmoEvaluation/'
+segment_loc = "/Users/sharingan/Documents/IEMOCAP_vid_clips/"
+
+
+# In[46]:
+
+
+# For each session go though each video and segment them into clips based on the time given in text filesf
+for sess in sessions:
+    # extract names of all videos
+    vid_full_names = [x for x in os.listdir(dataset_dir + sess + vid_loc) if x[-3:] == 'avi']
+    all_names = [x[:-4] for x in vid_full_names]
+    print "------------------------------------------------"
+    print sess
+#     print all_names
+    
+    # location to save the segmented video clips
+    os.system('mkdir ' + segment_loc + sess)
+    
+    for name in all_names:
+        vid_name = name + '.avi'
+        txt_file_name = name + '.txt'
+        
+        # location of video file and text file
+        vid_file = dataset_dir + sess + vid_loc + vid_name
+        txt_file = dataset_dir + sess + datafile_loc + txt_file_name
+        out_loc = segment_loc + sess + '/' # + respective_emotion_folder
+        
+        print "\nSegmentating ", vid_name
+        
+        with open(txt_file) as f:
+            data = f.readlines()
+        data = iter(data)
+        
+        # Extracting information from the text file and segmentation
+        try:
+            for line in data:
+                if line != '\n':
+                    continue;
+                line = next(data)
+                start_time = line.split()[0][1:]
+                end_time = line.split()[2][:-1]
+                segment_name = line.split()[3]
+                emotion = line.split()[4]
+                if emotion != 'xxx':
+                    os.system('mkdir ' + out_loc + emotion)
+#                     print line
+                    cmd = 'ffmpeg -i ' + vid_file + ' -ss ' + start_time + ' -acodec copy -to '+ end_time + ' ' + out_loc + emotion + '/' + segment_name + '.avi'
+#                     print cmd
+                    print os.system(cmd),
+        except(StopIteration):
+            None
